a525d8c8ad25c9445bae2984f2afdc21df5139aa
Spencer Jensen <sjensen@deseretdigital.com>
2019-11-15 14:28:13 -0700

    Update shame.js

--- functions/shame.js
+++ functions/shame.js
@@ -1,5 +1,6 @@
 exports.handler = function(event, context, callback){
   let jsonString = event.body;
+  console.log(event.body);
   let jsonObj = JSON.parse(jsonString);
   callback(null, {
     statusCode: 200,
