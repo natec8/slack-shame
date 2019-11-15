8c9d59941df291d949e7e49aa201a80cdef4e5c3
Spencer Jensen <sjensen@deseretdigital.com>
2019-11-15 14:33:23 -0700

    Update shame.js

--- functions/shame.js
+++ functions/shame.js
@@ -1,9 +1,12 @@
+let querystring = require('querystring');
+
 exports.handler = function(event, context, callback){
-  let jsonString = event.body;
-  console.log(event.body);
-  let jsonObj = JSON.parse(jsonString);
+  let body = event.body;
+  let bodyObj = querystring.parse(body);
+  console.log(bodyObj);
+
   callback(null, {
     statusCode: 200,
-    body: jsonObj.text
+    body: bodyObj.text
   });
 };
