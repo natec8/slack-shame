5d8933204b2f6566e28c2c5d530ab72afd34aaee
Spencer Jensen <sjensen@deseretdigital.com>
2019-11-15 14:55:58 -0700

    Update shame.js

--- functions/shame.js
+++ functions/shame.js
@@ -1,19 +1,27 @@
-let querystring = require('querystring');
+const querystring = require('querystring');
+const fetch = require("node-fetch");
 
 exports.handler = function(event, context, callback){
   let body = event.body;
   let bodyObj = querystring.parse(body);
+  let responseUrl = bodyObj.response_url;
   let response = {
       "response_type": "in_channel",
       "text": bodyObj.text,
   };
   response = JSON.stringify(response);
 
-  callback(null, {
+  const response = fetch(responseUrl, {
+    method: "post",
     statusCode: 200,
     headers: {
-      'Content-type': 'application/json',
+        "Content-type": "application/json",
     },
-    body: response
+    body: response,
+});
+  
+  callback(null, {
+    statusCode: 200,
+    body: "shame sent".
   });
 };
